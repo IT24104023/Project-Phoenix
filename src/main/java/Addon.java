c6eefc01b64be81e7f162a6e1a976156fb96ddfd
IT24100186 <it24100186@my.sliit.lk>
2025-05-13 00:57:41 +0530

    Update Addon.java

---
+++ src/main/java/Addon.java
@@ -0,0 +1,23 @@
+public class Addon {
+    private String addonId;
+    private String name;
+    private double price;
+
+    public Addon(String addonId, String name, double price) {
+        this.addonId = addonId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getAddonId() {
+        return this.addonId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+  }
